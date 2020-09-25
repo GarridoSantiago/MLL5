65fb86fc9d9ce81cd5aa7a32093b981f0ddecf6a
GarridoSantiago <69686650+GarridoSantiago@users.noreply.github.com>
2020-09-24 20:58:51 -0500

    version 2 valid_date

--- valid_date.m
+++ valid_date.m
@@ -13,27 +13,29 @@ elseif m>12
     valid = false;
     %error ('not valid month')
     return
-    
-elseif (rem(y,4)==0 && rem(y,100)~=0) || rem(y,400)==0
-    valid=leap(y,m,d);
-    
-else
-    valid = normal(y,m,d);
-    
-end
 
-function valid = leap(y,m,d)
-if(sum(m==[1 3 5 7 8 10 12])==1) && (d>31)
-    %fprintf ('no valid day number!!!!!!!!!!\n')
+elseif(sum(m==[1 3 5 7 8 10 12])==1) && (d>31)
+    %error ('no valid day number!!!!!!!!!!\n')
     valid=false;
     return
     
 elseif (sum(m==[4 6 9 11])==1) && (d>30)
-    %fprintf ('no valid day number!!!!!!!!!!\n')
+    %error ('no valid day number!!!!!!!!!!\n')
     valid=false;
     return
     
-elseif m==2 && (d>29)
+elseif ((rem(y,4)==0 && rem(y,100)~=0) || rem(y,400)==0) && m==2
+    valid = leap(y,m,d);
+    
+elseif ((rem(y,4)~=0 && rem(y,100)==0) || rem(y,400)~=0) && m==2
+    valid = normal(y,m,d);
+    
+else
+    valid = true;
+end
+
+function valid = leap(y,m,d)    
+if d>29
     %fprintf ('no valid day number!!!!!!!!!!\n')
     valid=false;
     return
@@ -45,17 +47,7 @@ else
 end
 
 function valid = normal(y,m,d)
-if(sum(m==[1 3 5 7 8 10 12])==1) && (d>31)
-    %fprintf ('no valid day number!!!!!!!!!!\n')
-    valid=false;
-    return
-    
-elseif (sum(m==[4 6 9 11])==1) && (d>30)
-    %fprintf ('no valid day number!!!!!!!!!!\n')
-    valid=false;
-    return
-    
-elseif m==2 && (d>28)
+if d>28
     %fprintf ('no valid day number!!!!!!!!!!\n')
     valid=false;
     return
