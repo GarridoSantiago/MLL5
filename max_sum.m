5fd0ac4a7955f03a953a3c26245b088d51ef9d32
GarridoSantiago <69686650+GarridoSantiago@users.noreply.github.com>
2020-10-02 13:23:31 -0500

    max_sum comments

--- max_sum.m
+++ max_sum.m
@@ -1,11 +1,14 @@
 function [summa,index] = max_sum(v,n)
-%
+% la siguiente linea hace un "array" vacio esto es para el condicional pues
+% un cero no funcionaria si la suma total es negativa
 summa=int16.empty;
 if n>length(v)
     summa=0; index=-1;
     return
 end
 for ii = 1:(length(v)-(n-1))
+    %El condicional corre el bloque de codigo si "summa" está vacio o si es
+    %menor a la suma total
     if isempty(summa) || summa < sum(v(ii:(ii+(n-1))))
         summa = sum(v(ii:(ii+(n-1))));
         index = ii;
